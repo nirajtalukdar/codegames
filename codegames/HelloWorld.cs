5737ca2c4480f383dc1dfe2c54566106aee5c4eb
Niraj Talukdar <nirajtalukdar@gmail.com>
2017-11-16 01:32:42 +0000

    first csharp commit

---
+++ codegames/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Utils {
+    
+    public class HelloWorld
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello World !! Welcome to C Sharp");
+        }  
+    }
+
+}
